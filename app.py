b193be8b141db92410f3e0507c905f5f6fc04930
67070151 <67070151@kmitl.ac.th>
2024-11-01 01:16:59 +0700

    add .gitignore file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -7,17 +7,20 @@ app = Flask(__name__)
 
 # Set configurations
 app.config['SECRET_KEY'] = os.urandom(24)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/muscal'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:GWWoGUnWEbzBYRdkqmbfnRSLnOTiAkOv@autorack.proxy.rlwy.net:39603/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
 
-# Import routes
+# Import routes (ensure you have a routes.py file with defined routes)
 from routes import *
+
+# Handler for Vercel serverless functions
+def handler(environ, start_response):
+    return app(environ, start_response)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  # Create the database tables
     app.run(debug=True)
